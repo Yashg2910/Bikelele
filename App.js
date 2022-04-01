5ae96099a2d625cea8e1fa2d620926f0a751471a
Yash G <yash@cerah.co>
2022-04-01 19:08:47 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <WebView 
+      style={styles.container}
+      source={{ uri: 'https://bikelele.com/' }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
