9c2779dd453ca1b855f4e8ba0973edb2a2a50ab6
Yash G <yash@cerah.co>
2022-04-04 13:01:29 +0530

    Added back press handler to go back to previous web page. Added spinner for before loading webpages.

--- App.js
+++ App.js
@@ -1,13 +1,46 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import { StyleSheet, BackHandler, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
+  const webview = useRef(null);
+  const [spinnerVisible, setSpinnerVisible] = useState(true);
+
+  const onAndroidBackPress = () => {
+    if (webview.current) {
+      webview.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
+    };
+  }, []);
+
   return (
-    <WebView 
-      style={styles.container}
-      source={{ uri: 'https://bikelele.com/' }}
-    />
+    <>
+      <WebView 
+        style={styles.container}
+        source={{ uri: 'https://bikelele.com/' }}
+        ref={webview}
+        onLoadProgress={({nativeEvent}) => {
+          if (nativeEvent.progress < 1) {
+            setSpinnerVisible(true);
+          }
+        }}
+        onLoadStart={() => setSpinnerVisible(true)}
+        onLoadEnd={() => setSpinnerVisible(false)}
+      />
+      {spinnerVisible &&
+        <ActivityIndicator
+          style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}} 
+          size="large" 
+          color="#f9b800"/>}
+    </>
   );
 }
 
