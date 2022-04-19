d95172f78489756449029a6316285390c41e4c63
Yash G <yash@cerah.co>
2022-04-19 11:23:02 +0530

    Added expo push notification to uri.

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { StyleSheet, BackHandler, ActivityIndicator } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  const baseUri = "https://bikelele.com/";
   const webview = useRef(null);
   const [spinnerVisible, setSpinnerVisible] = useState(true);
+  const [uri, setUri] = useState(`${baseUri}`);
 
   const onAndroidBackPress = () => {
     if (webview.current) {
@@ -15,6 +19,10 @@ export default function App() {
   };
 
   useEffect(() => {
+    registerForPushNotificationsAsync().then(token => {
+      setUri(`${baseUri}?dtoken=${token}`);
+    });
+
     BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
     return () => {
       BackHandler.removeEventListener('hardwareBackPress', onAndroidBackPress);
@@ -25,7 +33,7 @@ export default function App() {
     <>
       <WebView 
         style={styles.container}
-        source={{ uri: 'https://bikelele.com/' }}
+        source={{ uri }}
         ref={webview}
         onLoadProgress={({nativeEvent}) => {
           if (nativeEvent.progress < 1) {
@@ -42,6 +50,37 @@ export default function App() {
           color="#f9b800"/>}
     </>
   );
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+    if (Device.isDevice) {
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+      }
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+      console.log(token);
+    } else {
+      alert('Must use physical device for Push Notifications');
+    }
+  
+    if (Platform.OS === 'android') {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: '#FF231F7C',
+      });
+    }
+  
+    return token;
+  }
 }
 
 const styles = StyleSheet.create({
